fb0603cfff1bffe9419dbde830c6b8b2ed1694da
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-19 14:12:23 +0400

    Add basic files and math function

---
+++ scripts.js
@@ -0,0 +1,15 @@
+const add = function (firstOp, secondOp) {
+  return firstOp + secondOp;
+};
+
+const subtract = function (firstOp, secondOp) {
+  return firstOp - secondOp;
+};
+
+const multiply = function (firstOp, secondOp) {
+  return firstOp * secondOp;
+};
+
+const divide = function (firstOp, secondOp) {
+  return firstOp / secondOp;
+};
