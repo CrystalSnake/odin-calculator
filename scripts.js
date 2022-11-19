36c3132cd8834758207c098896988891e6daeff3
CrystalSnake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-19 14:20:29 +0400

    Add operate function

--- scripts.js
+++ scripts.js
@@ -13,3 +13,21 @@ const multiply = function (firstOp, secondOp) {
 const divide = function (firstOp, secondOp) {
   return firstOp / secondOp;
 };
+
+const operate = function (firstOp, operator, secondOp) {
+  switch (operator) {
+    case '+':
+      console.log(add(firstOp, secondOp));
+      break;
+    case '-':
+      console.log(subtract(firstOp, secondOp));
+      break;
+    case '*':
+      console.log(multiply(firstOp, secondOp));
+      break;
+    case '/':
+      console.log(divide(firstOp, secondOp));
+  }
+};
+
+operate(5, '*', 6);
