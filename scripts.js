a76a282df500ff56ddb1e9663fccb7443b03bf1c
Crystal Snake <22963880+CrystalSnake@users.noreply.github.com>
2022-11-20 18:57:10 +0400

    Link plus event listener with add function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+let firstOp;
+let secondOp;
+let operand;
+let result;
+
 const add = function (firstOp, secondOp) {
   return firstOp + secondOp;
 };
@@ -49,68 +54,86 @@ const clear = document.getElementById('clear');
 
 const screen = document.getElementById('print');
 
-let displayValue = 0;
+let displayValue = '';
 
 const printKeyValue = function () {
   zero.addEventListener('click', () => {
-    displayValue = 0;
+    displayValue = '0';
     screen.textContent += displayValue;
   });
   one.addEventListener('click', () => {
-    displayValue = 1;
-    screen.textContent += displayValue;
+    displayValue += '1';
+    screen.textContent = displayValue;
   });
   two.addEventListener('click', () => {
-    displayValue = 2;
-    screen.textContent += displayValue;
+    displayValue += '2';
+    screen.textContent = displayValue;
   });
   three.addEventListener('click', () => {
-    displayValue = 3;
-    screen.textContent += displayValue;
+    displayValue += '3';
+    screen.textContent = displayValue;
   });
   four.addEventListener('click', () => {
-    displayValue = 4;
-    screen.textContent += displayValue;
+    displayValue += '4';
+    screen.textContent = displayValue;
   });
   five.addEventListener('click', () => {
-    displayValue = 5;
-    screen.textContent += displayValue;
+    displayValue += '5';
+    screen.textContent = displayValue;
   });
   six.addEventListener('click', () => {
-    displayValue = 6;
-    screen.textContent += displayValue;
+    displayValue += '6';
+    screen.textContent = displayValue;
   });
   seven.addEventListener('click', () => {
-    displayValue = 7;
-    screen.textContent += displayValue;
+    displayValue += '7';
+    screen.textContent = displayValue;
   });
   eight.addEventListener('click', () => {
-    displayValue = 8;
-    screen.textContent += displayValue;
+    displayValue += '8';
+    screen.textContent = displayValue;
   });
   nine.addEventListener('click', () => {
-    displayValue = 9;
-    screen.textContent += displayValue;
+    displayValue += '9';
+    screen.textContent = displayValue;
   });
   dot.addEventListener('click', () => {
-    displayValue = '.';
-    screen.textContent += displayValue;
+    displayValue += '.';
+    screen.textContent = displayValue;
   });
   plus.addEventListener('click', () => {
-    displayValue = '+';
-    screen.textContent += displayValue;
+    //operand = '+';
+    if (firstOp === undefined) {
+      firstOp = +displayValue;
+      console.log(`first ${firstOp}`);
+    } else {
+      secondOp = +displayValue;
+      console.log(`second ${secondOp}`);
+      if (secondOp) {
+        result = add(firstOp, secondOp);
+        console.log(result);
+        displayValue = result;
+        screen.textContent = displayValue;
+        firstOp = result;
+        secondOp = '';
+      }
+    }
+    displayValue = '';
   });
   minus.addEventListener('click', () => {
-    displayValue = '-';
-    screen.textContent += displayValue;
+    screen.textContent += '-';
+    firstOp = +displayValue;
+    console.log(firstOp);
   });
   timesSign.addEventListener('click', () => {
-    displayValue = '*';
-    screen.textContent += displayValue;
+    screen.textContent += 'x';
+    firstOp = +displayValue;
+    console.log(firstOp);
   });
   division.addEventListener('click', () => {
-    displayValue = '/';
-    screen.textContent += displayValue;
+    screen.textContent += '/';
+    firstOp = +displayValue;
+    console.log(firstOp);
   });
   clear.addEventListener('click', () => {
     displayValue = '';
